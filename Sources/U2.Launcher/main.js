477bb960644edfc7060d4ff1b459814e93feebfd
Serhii Usmanov <serhii.usmanov@materialise.kiev.ua>
2025-09-12 01:06:53 +0300

    Disabled opening of the DevTools panel

--- Sources/U2.Launcher/main.js
+++ Sources/U2.Launcher/main.js
@@ -16,7 +16,7 @@ function createWindow() {
     win.loadFile('index.html');
 
     // Open the DevTools.
-    win.webContents.openDevTools();
+    // win.webContents.openDevTools();
 }
 
 app.whenReady().then(createWindow);
