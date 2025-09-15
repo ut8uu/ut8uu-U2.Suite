bcaabc0fa010f58351f88a7e7a6c93e1771f655b
Serhii Usmanov <serhii.usmanov@materialise.kiev.ua>
2025-09-15 23:06:18 +0300

    Extended the QSO table

--- Sources/U2.Logger.Backend/Migrations/20250912155201_InitialCreate.cs
+++ Sources/U2.Logger.Backend/Migrations/20250912155201_InitialCreate.cs
@@ -1,43 +1,63 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Microsoft.EntityFrameworkCore.Migrations;
 
-#nullable disable
+namespace U2.Logger.Backend.Migrations;
 
-namespace U2.Logger.Backend.Migrations
+public partial class InitialCreate : Migration
 {
-    /// <inheritdoc />
-    public partial class InitialCreate : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.CreateTable(
-                name: "QSOs",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "INTEGER", nullable: false)
-                        .Annotation("Sqlite:Autoincrement", true),
-                    Callsign = table.Column<string>(type: "TEXT", nullable: false),
-                    Snt = table.Column<string>(type: "TEXT", nullable: false),
-                    Rcvd = table.Column<string>(type: "TEXT", nullable: false),
-                    Name = table.Column<string>(type: "TEXT", nullable: false),
-                    Comment = table.Column<string>(type: "TEXT", nullable: false),
-                    Band = table.Column<string>(type: "TEXT", nullable: false),
-                    Mode = table.Column<string>(type: "TEXT", nullable: false),
-                    Propagation = table.Column<string>(type: "TEXT", nullable: false),
-                    DateTime = table.Column<DateTime>(type: "TEXT", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_QSOs", x => x.Id);
-                });
-        }
+        migrationBuilder.CreateTable(
+            name: "QSOs",
+            columns: table => new
+            {
+                Id = table.Column<int>(type: "INTEGER", nullable: false)
+                    .Annotation("Sqlite:Autoincrement", true),
+                Band = table.Column<string>(name: "band", type: "TEXT", nullable: true),
+                BandRx = table.Column<string>(name: "band_rx", type: "TEXT", nullable: true),
+                Callsign = table.Column<string>(name: "call", type: "TEXT", nullable: true),
+                Comment = table.Column<string>(name: "comment", type: "TEXT", nullable: true),
+                Continent = table.Column<string>(name: "cont", type: "TEXT", nullable: true),
+                Country = table.Column<string>(name: "country", type: "TEXT", nullable: true),
+                CqZone = table.Column<string>(name: "cqz", type: "TEXT", nullable: true),
+                Distance = table.Column<int>(name: "distance", type: "INTEGER", nullable: true),
+                Dxcc = table.Column<string>(name: "dxcc", type: "TEXT", nullable: true),
+                Email = table.Column<string>(name: "email", type: "TEXT", nullable: true),
+                Freq = table.Column<double>(name: "freq", type: "REAL", nullable: true),
+                FreqRx = table.Column<double>(name: "freq_rx", type: "REAL", nullable: true),
+                Gridsquare = table.Column<string>(name: "gridsquare", type: "TEXT", nullable: true),
+                ItuZone = table.Column<string>(name: "ituz", type: "TEXT", nullable: true),
+                Lat = table.Column<string>(name: "lat", type: "TEXT", nullable: true),
+                Lon = table.Column<string>(name: "lon", type: "TEXT", nullable: true),
+                Mode = table.Column<string>(name: "mode", type: "TEXT", nullable: true),
+                MyCity = table.Column<string>(name: "my_city", type: "TEXT", nullable: true),
+                MyCountry = table.Column<string>(name: "my_country", type: "TEXT", nullable: true),
+                MyCqZone = table.Column<string>(name: "my_cq_zone", type: "TEXT", nullable: true),
+                MyGridsquare = table.Column<string>(name: "my_gridsquare", type: "TEXT", nullable: true),
+                MyItuZone = table.Column<string>(name: "my_itu_zone", type: "TEXT", nullable: true),
+                MyLat = table.Column<string>(name: "my_lat", type: "TEXT", nullable: true),
+                MyLon = table.Column<string>(name: "my_lon", type: "TEXT", nullable: true),
+                MyName = table.Column<string>(name: "my_name", type: "TEXT", nullable: true),
+                Name = table.Column<string>(name: "name", type: "TEXT", nullable: true),
+                Operator = table.Column<string>(name: "operator", type: "TEXT", nullable: true),
+                QslRcvd = table.Column<string>(name: "qsl_rcvd", type: "TEXT", nullable: true),
+                QslSent = table.Column<string>(name: "qsl_sent", type: "TEXT", nullable: true),
+                QslVia = table.Column<string>(name: "qsl_via", type: "TEXT", nullable: true),
+                DateTime = table.Column<string>(name: "date_time", type: "TEXT", nullable: true),
+                DateTimeOff = table.Column<string>(name: "date_time_off", type: "TEXT", nullable: true),
+                Qth = table.Column<string>(name: "qth", type: "TEXT", nullable: true),
+                RstRcvd = table.Column<string>(name: "rst_rcvd", type: "TEXT", nullable: true),
+                RstSent = table.Column<string>(name: "rst_sent", type: "TEXT", nullable: true),
+                StationCallsign = table.Column<string>(name: "station_callsign", type: "TEXT", nullable: true),
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_QSOs", x => x.Id);
+            });
+    }
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropTable(
-                name: "QSOs");
-        }
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropTable(
+            name: "QSOs");
     }
 }
--- Sources/U2.Logger.Backend/Models/QSO.cs
+++ Sources/U2.Logger.Backend/Models/QSO.cs
@@ -1,15 +1,119 @@
-﻿namespace U2.Logger.Backend.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace U2.Logger.Backend.Models;
 
 public class QSO
 {
-    public int Id { get; set; } // Primary key
-    public string Callsign { get; set; }
-    public string Snt { get; set; }
-    public string Rcvd { get; set; }
-    public string Name { get; set; }
-    public string Comment { get; set; }
-    public string Band { get; set; }
-    public string Mode { get; set; }
-    public string Propagation { get; set; }
-    public DateTime DateTime { get; set; }
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    [Column("band")]
+    public string? Band { get; set; }
+
+    [Column("band_rx")]
+    public string? BandRx { get; set; }
+
+    [Column("call")]
+    public string? Callsign { get; set; }
+
+    [Column("comment")]
+    public string? Comment { get; set; }
+
+    [Column("cont")]
+    public string? Continent { get; set; }
+
+    [Column("country")]
+    public string? Country { get; set; }
+
+    [Column("cqz")]
+    public string? CqZone { get; set; }
+
+    [Column("distance")]
+    public int? Distance { get; set; }
+
+    [Column("dxcc")]
+    public string? Dxcc { get; set; }
+
+    [Column("email")]
+    public string? Email { get; set; }
+
+    [Column("freq")]
+    public double? Freq { get; set; }
+
+    [Column("freq_rx")]
+    public double? FreqRx { get; set; }
+
+    [Column("gridsquare")]
+    public string? Gridsquare { get; set; }
+
+    [Column("ituz")]
+    public string? ItuZone { get; set; }
+
+    [Column("lat")]
+    public string? Lat { get; set; }
+
+    [Column("lon")]
+    public string? Lon { get; set; }
+
+    [Column("mode")]
+    public string? Mode { get; set; }
+
+    [Column("my_city")]
+    public string? MyCity { get; set; }
+
+    [Column("my_country")]
+    public string? MyCountry { get; set; }
+
+    [Column("my_cq_zone")]
+    public string? MyCqZone { get; set; }
+
+    [Column("my_gridsquare")]
+    public string? MyGridsquare { get; set; }
+
+    [Column("my_itu_zone")]
+    public string? MyItuZone { get; set; }
+
+    [Column("my_lat")]
+    public string? MyLat { get; set; }
+
+    [Column("my_lon")]
+    public string? MyLon { get; set; }
+
+    [Column("my_name")]
+    public string? MyName { get; set; }
+
+    [Column("name")]
+    public string? Name { get; set; }
+
+    [Column("operator")]
+    public string? Operator { get; set; }
+
+    [Column("qsl_rcvd")]
+    public string? QslRcvd { get; set; }
+
+    [Column("qsl_sent")]
+    public string? QslSent { get; set; }
+
+    [Column("qsl_via")]
+    public string? QslVia { get; set; }
+
+    [Column("date_time")]
+    public string? DateTime { get; set; }
+
+    [Column("date_time_off")]
+    public string? DateTimeOff { get; set; }
+
+    [Column("qth")]
+    public string? Qth { get; set; }
+
+    [Column("rst_rcvd")]
+    public string? RstRcvd { get; set; }
+
+    [Column("rst_sent")]
+    public string? RstSent { get; set; }
+
+    [Column("station_callsign")]
+    public string? StationCallsign { get; set; }
 }
