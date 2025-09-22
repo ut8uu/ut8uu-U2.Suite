c51f46d951909c5c792fc5d948ce88b70afe8992
Serhii Usmanov <serhii.usmanov@materialise.kiev.ua>
2025-09-22 22:59:46 +0300

    Added U2.Logger.Core project

---
+++ Sources/U2.Logger.Core/QSO.cs
@@ -0,0 +1,42 @@
+﻿namespace U2.Logger.Core.Models;
+
+public class QSO
+{
+    public int Id { get; set; }
+    public string? Band { get; set; }
+    public string? BandRx { get; set; }
+    public string? Callsign { get; set; }
+    public string? Comment { get; set; }
+    public string? Continent { get; set; }
+    public string? Country { get; set; }
+    public string? CqZone { get; set; }
+    public long? DateTime { get; set; }
+    public long? DateTimeOff { get; set; }
+    public int? Distance { get; set; }
+    public string? Dxcc { get; set; }
+    public string? Email { get; set; }
+    public double? Freq { get; set; }
+    public double? FreqRx { get; set; }
+    public string? Gridsquare { get; set; }
+    public string? ItuZone { get; set; }
+    public string? Lat { get; set; }
+    public string? Lon { get; set; }
+    public string? Mode { get; set; }
+    public string? MyCity { get; set; }
+    public string? MyCountry { get; set; }
+    public string? MyCqZone { get; set; }
+    public string? MyGridsquare { get; set; }
+    public string? MyItuZone { get; set; }
+    public string? MyLat { get; set; }
+    public string? MyLon { get; set; }
+    public string? MyName { get; set; }
+    public string? Name { get; set; }
+    public string? Operator { get; set; }
+    public string? QslRcvd { get; set; }
+    public string? QslSent { get; set; }
+    public string? QslVia { get; set; }
+    public string? Qth { get; set; }
+    public string? RstRcvd { get; set; }
+    public string? RstSent { get; set; }
+    public string? StationCallsign { get; set; }
+}
